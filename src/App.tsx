e3993a45716d5795b04f5a3799bf9fd4c76a67de
Davo <omarcorreonova@gmail.com>
2025-10-03 09:27:21 -0600

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./features/schoolClasses/components/header";
+import SchoolClassList from "./features/schoolClasses/pages/SchoolClassList";
+import Activities from "./features/activities/pages/Activities";
+import Teams from "./features/teams/pages/Teams";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    </>
-  )
+	return (
+		<Router>
+			<Header />
+			<Routes>
+				{/*<Route path="/" element={<Home />} />*/}
+				<Route path="/schoolclasses" element={<SchoolClassList />} />
+				<Route path="/teams" element={<Teams />} />
+				<Route path="/activities" element={<Activities />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App
---
+++ src/features/activities/pages/Activities.tsx
@@ -0,0 +1,6 @@
+
+export default function Activities() {
+	return (
+		<div></div>
+	);
+}
